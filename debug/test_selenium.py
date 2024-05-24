aec854f5b60a4216cb8469b11f1ba1ff6da9d652
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-05-24 00:59:36 -0400

    Update test_selenium.py

--- debug/test_selenium.py
+++ debug/test_selenium.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
-
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))
+display.start()
 chromedriver_autoinstaller.install()
+
+chrome_options = webdriver.ChromeOptions()
+options = [
+    # Define window size here
+    "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+
+    # "--headless",
+    # "--disable-gpu",
+    # "--window-size=1920,1200",
+    # "--ignore-certificate-errors",
+    # "--disable-extensions",
+    # "--no-sandbox",
+    # "--disable-dev-shm-usage",
+    # '--remote-debugging-port=9222'
+]
+for option in options:
+    chrome_options.add_argument(option)
+
 
 # Create a new instance of the Chrome driver
 # I tried, the headless driver doesn't work
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options = chrome_options)
 
 # Navigate to the website
 driver.get("https://www.unitedstateszipcodes.org/20036/")
